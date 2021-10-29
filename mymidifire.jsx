947dd7f01df2195258c00bbca314c0cd6dde36c9
S_Rikuto <rikutonsicloud@gmail.com>
2021-10-29 14:04:43 +0900

    Change to class structure with MidiReader

--- mymidifire.jsx
+++ mymidifire.jsx
@@ -18,66 +18,104 @@
 	var executeButton = parentGroup.add("button",undefined, "Load");
 	executeButton.onClick = function(){
 		window.close();
-		loadMidi(midiPathText.text);
+		onLoadButtonClicked(midiPathText.text);
 	}
 
 	window.center();
 	window.show();
 }
 
-function readFile(path)
-{
-	var f = new File(path);
-	f.encoding = "BINARY";
+class MidiReader{
+	getStr(isTrack) {
+		if (isTrack)
+			return this.tracks[this.trackIndex];
+		else
+			return this.file;
+	}
 
-	f.open ("r");
-	var length = f.length;
-	var result = f.read(length);
-	f.close();
-	return result;
-}
+	getNum(length, isTrack = false) {
+		var str = this.getStr(isTrack);
+		var ret = 0;
+		for (var i = 0; i < length; i++) {
+			ret <<= 8;
+			ret |= this.file.charCodeAt(this.index + i);
+		}
 
-function loadMidi(path){
-	var file = readFile(path);
-	var foralert = ""
-	var trackNum = file.charCodeAt(0x0b);
-	foralert += trackNum;
-	var itr = 0x12;
-	var tracks = new Array();
-	for (var i = 0; i < trackNum; i++) {
-		var length = getBinaryNum(file, itr, itr + 4);
-		alert(length);
-		tracks.push(file.substring(itr + 8, itr + length + 4));
-		itr += length + 8;
-	};
-	alert(tracks[0]);
-	alert(foralert);
-	for (var i in tracks) {
-		var t = 0;
-		for (var j=0, len=tracks[i].length; j < len;) {
-			if( tracks[i].charCodeAt(j) & 0x80  != 0){
-			  t += getBinaryNum(tracks, j, j + 2) - 0xf000;
-			  j += 2;
+		return ret;
+	}
+
+	getFlexibleNum(isTrack = false) {
+		var num;
+		if (this.getNum(1, isTrack) & 0x80 != 0) {
+			num = getNum(2, isTrack) - 0xf000;
+			this.index += 2;
+		}
+		else {
+			num = getNum(1, isTrack);
+			this.index++;
+		}
+		return num;
+	}
+
+	constructor() {
+		this.file = "";
+		this.tracks = [];
+		this.trackNum = -1;
+		this.trackIndex = 0;
+		this.index = 0;
+	}
+
+	load(path) {
+		var f = new File(path);
+		f.encoding = "BINARY";
+
+		f.open("r");
+		var length = f.length;
+		this.file = f.read(length);
+		f.close();
+	}
+
+	divideTracks() {
+		index = 0x12;
+		for (var i = 0; i < trackNum; i++) {
+			var length = getNum(4);
+			alert(length);
+			this.tracks.push(file.substring(index + 8, index + length + 4));
+			index += length + 8;
+		}
+	}
+
+	// WIP
+	readLayerNames() {
+		for (var i in tracks) {
+			var t = 0;
+			for (var j = 0, len = tracks[i].length; j < len;) {
+				var dt;
+				[dt, j] = getFlexibleNum(tracks[i], j);
+				t += dt;
+
+				var event = tracks[i].charCodeAt(j);
+				j++;
+				if (event == 0xf0 || event == 0xf7) {
+					var length = this.getFlexibleNum(true);
+					alert("SysEx");
+				}
 			}
-			else{
-			  t += getBinaryNum(tracks, j, j + 1);
-			  j++;
-			}
-			  
-		};
-	  
-	};
+		}
+	}
 }
 
-function getBinaryNum(file, start, end){
-	var l = 0;
-	for (var i = start; i < end; i++) {
-		l <<= 8;
-		l |= file.charCodeAt(i);;
-	};
-	return l;
-}
-
-
+// main
+var midiReader;
 setting1();
 
+// main 2(When Load button Pushed)
+function onLoadButtonClicked(path) {
+	midiReader = new MidiReader();
+	midiReader.load(path);
+}
+
+// main 3(When Track selected)
+function onSelectButtonClicked() {
+	
+}
